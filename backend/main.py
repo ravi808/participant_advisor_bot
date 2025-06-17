429b38c8c0237988d1f8c830e912233b0557ab33
ravi808 <rvimittal@gmail.com>
2025-06-17 17:12:13 +0530

    application running testing

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from api.advisor_router import advisor_router
+from api.advisor_router import router
 from models.config import settings
 
 app = FastAPI(title="Participant Advisor Bot")
-app.include_router(advisor_router, prefix="/advisor")
+app.include_router(router, prefix="/advisor")
